c381945f3c626e715daa891c399dc6ae907433ed
Hoang Van Hiep <hiepthanhxa123@gmail.com>
2025-02-14 16:54:20 +0700

    baitap

---
+++ shop_demo/src/test/java/bu3_training/shop_demo/ShopDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package bu3_training.shop_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
